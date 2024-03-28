e1c9173e6ee3dca21a4bf65fe32b8b90d1e7085a
isaac <n.wool90@gmail.com>
2024-03-28 23:01:28 +0900

    findExactSong

--- src/domains/AppleMusicDomain.ts
+++ src/domains/AppleMusicDomain.ts
@@ -3,6 +3,7 @@ import * as config from 'config';
 import got from 'got';
 
 import { SearchTypes } from './constants';
+import { IAppleMusicSearchResponse, IAppleMusicSongDetailData } from './type';
 
 @Service()
 export default class AppleMusicDomain {
@@ -10,28 +11,58 @@ export default class AppleMusicDomain {
     private readonly token: string = config.get('appleMusic.token');
     private readonly pageSize: number = 10;
 
-    async requestByPath(path: string) {
+    private async requestByPath(path: string) {
         const response = await got.get(`${this.baseUrl}${path}`, {
             headers: {
                 Authorization: `Bearer ${this.token}`,
             },
+            resolveBodyOnly: true,
         });
 
-        return JSON.parse(response.body);
+        return JSON.parse(response);
     }
 
-    public async search(countryCode: string, keyword: string, types: SearchTypes[]) {
+    public async search(
+        countryCode: string,
+        keyword: string,
+        types: SearchTypes[]
+    ): Promise<IAppleMusicSearchResponse> {
         const response = await got.get(`${this.baseUrl}/v1/catalog/${countryCode}/search`, {
             headers: {
                 Authorization: `Bearer ${this.token}`,
             },
             searchParams: {
-                term: keyword,
+                term: keyword.replace(/ /g, '+'),
                 limit: this.pageSize,
                 types: types.join(','),
             },
+            resolveBodyOnly: true,
         });
 
-        return JSON.parse(response.body);
+        return JSON.parse(response);
+    }
+
+    public async findExactSong(songName: string, isrc: string): Promise<IAppleMusicSongDetailData | null> {
+        const response = await this.search('US', songName, [SearchTypes.SONGS]);
+        let songs = response.results.songs?.data || [];
+        let exactMatch = songs.find((song) => song.attributes.isrc === isrc);
+        let nextUrl = response.results.songs?.next;
+
+        if (exactMatch) {
+            return exactMatch;
+        }
+
+        while (nextUrl) {
+            const nextResponse = await this.requestByPath(nextUrl);
+            songs = nextResponse.results.songs?.data || [];
+            exactMatch = songs.find((song) => song.attributes.isrc === isrc);
+            nextUrl = nextResponse.results.songs?.next;
+
+            if (exactMatch) {
+                return exactMatch;
+            }
+        }
+
+        return null;
     }
 }
